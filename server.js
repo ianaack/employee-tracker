d0f3eba9f0b7345a76b8947f62600f2d2d6d7f95
ianaack <ianaack@gmail.com>
2022-04-30 14:58:21 -0400

    Connect to MySQL database successful

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
+// required modules
+const cTable = require("console.table");
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
 
+// required files
+const credentials = require("./db/credentials");
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: credentials.user,
+  password: credentials.password,
+  database: "company",
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.log("Error connecting to the MySQL Database");
+    return;
+  }
+  console.log("Connection to the MySQL Database successful");
+});
