4a843a19ba55f4c6a1299968b01554a842ae9d4d
ianaack <ianaack@gmail.com>
2022-04-30 22:23:38 -0400

    Feature: Insert Starter Data
    added INSERT INTO values in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,5 +16,16 @@ INSERT INTO roles (title, salary, department_id)
   ("Legal Team Lead", 250000, 4),
   ("Lawyer", 190000, 4);
 
-INSERT INTO employees (first_name, last_name, )
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+  VALUES
+  ("John", "Doe", 1, NULL),
+  ("Mike", "Chan", 2, 1),
+  ("Ashley", "Rodriguez", 3, NULL),
+  ("Kevin", "Tupik", 4, 3),
+  ("Kunal", "Singh", 5, NULL),
+  ("Malia", "Brown", 6, 5),
+  ("Sarah", "Lourd", 7, NULL),
+  ("Tom", "Allen", 8, 7);
+
+
 
